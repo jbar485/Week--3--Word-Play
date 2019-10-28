dcea1a1bf305aa68b7c42e1c35171041422005a5
Student <example@example.com>
2019-10-28 16:21:48 -0700

    add a working product

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 var sentence = [];
 var three;
+var words;
 $(document).ready(function(){
   $("#wordForm").submit(function(event){
     event.preventDefault();
-    var words = $("input#entry").val().split(" ")
-    var three = words.map(function(word){
+    words = $("input#entry").val().split(" ");
+    var wordMap = words.map(function(word){
+      // console.log("type of 1: ", typeof(word));
       if(word.length >= 3){
-        $("p#final-sentence").append(word + " ");
-        console.log(word);
+        // console.log(word);
+        var reversedString = word.split("").reverse().join("");
+        $("p#final-sentence").append(reversedString + " ");
+        // console.log("type of 2: ", typeof(word));
+        // console.log("Wordmap: ", wordMap);
       }else {
-        word.replace("")
+        word.replace("");
         // console.log(three);
       }
     })
-    // var splitWords = words.map(function(){
-    //
-    // })
+    // three.join().reverse();
   });
 
 
