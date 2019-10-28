a73389640d97230d7e6359457270cc5975a4d992
Student <example@example.com>
2019-10-28 15:48:28 -0700

    succesfully append paragraph with words that are greater than or equal to 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
+var sentence = [];
+var three;
 $(document).ready(function(){
   $("#wordForm").submit(function(event){
     event.preventDefault();
     var words = $("input#entry").val().split(" ")
     var three = words.map(function(word){
       if(word.length >= 3){
+        $("p#final-sentence").append(word + " ");
         console.log(word);
+      }else {
+        word.replace("")
+        // console.log(three);
       }
     })
     // var splitWords = words.map(function(){
     //
     // })
-    console.log(words, three);
   });
 
 
